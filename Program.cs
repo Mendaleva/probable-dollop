d413dcfba38f6cb2a566d7b8eef583931a91cede
Mendaleva <159470370+Mendaleva@users.noreply.github.com>
2024-02-19 01:13:35 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,3 +9,44 @@ else
 {
     Console.WriteLine("Число не кратно");
 }
+
+// Задача 2: Напишите программу, 
+// которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+// в которой находится эта точка.
+
+int x = -3;
+int y = 2;
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
+
+// Задача 3: Напишите программу, 
+// которая принимает на вход целое число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+int number = 57;
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
+
